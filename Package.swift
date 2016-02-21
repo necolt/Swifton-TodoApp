49983b546788ee2b6832cab46731cdd6fafeda2f
Saulius Grigaitis <saulius@necolt.com>
2016-02-21 19:14:53 +0200

    Updated Swifton to 0.0.2

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Swifton-TodoApp",
     dependencies: [
-        .Package(url: "https://github.com/necolt/Swifton.git", versions: Version(0,0,1)..<Version(1,0,0)),
+        .Package(url: "https://github.com/necolt/Swifton.git", versions: Version(0,0,2)..<Version(1,0,0)),
         .Package(url: "https://github.com/necolt/Curassow.git", versions: Version(0,4,0)..<Version(1,0,0)),
     ]
 )
