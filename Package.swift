b028faaa9f5201999c65ee98dad36fa18d631b63
Saulius Grigaitis <saulius@necolt.com>
2016-03-19 19:52:26 +0200

    Updated to Swifton 0.0.7

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "Swifton-TodoApp",
     dependencies: [
         .Package(url: "https://github.com/necolt/Swifton.git", versions: Version(0,0,5)..<Version(1,0,0)),
-        .Package(url: "https://github.com/necolt/Curassow.git", versions: Version(0,4,0)..<Version(1,0,0)),
+        .Package(url: "https://github.com/necolt/Curassow.git", majorVersion: 0, minor: 4),
     ]
 )
--- Sources/TodosController.swift
+++ Sources/TodosController.swift
@@ -2,46 +2,46 @@ import Swifton
 
 class TodosController: ApplicationController { 
     var todo: Todo?    
-    override init() { super.init()
+    override func controller() {
 
     beforeAction("setTodo", ["only": ["show", "edit", "update", "destroy"]])
 
     action("index") { request in
         let todos = ["todos": Todo.allAttributes()]
-        return self.respondTo(request, [
-            "html": { self.render("Todos/Index", todos) },
-            "json": { self.renderJSON(todos) }
+        return respondTo(request, [
+            "html": { render("Todos/Index", todos) },
+            "json": { renderJSON(todos) }
         ])
     }
 
     action("show") { request in
-        return self.render("Todos/Show", self.todo)
+        return render("Todos/Show", self.todo)
     }
 
     action("new") { request in
-        return self.render("Todos/New")
+        return render("Todos/New")
     }
 
     action("edit") { request in
-        return self.render("Todos/Edit", self.todo)
+        return render("Todos/Edit", self.todo)
     } 
     action("create") { request in
         Todo.create(request.params)
-        return self.redirectTo("/todos")
+        return redirectTo("/todos")
     }
 
     action("update") { request in
         self.todo!.update(request.params)
-        return self.redirectTo("/todos/\(self.todo!.id)")
+        return redirectTo("/todos/\(self.todo!.id)")
     }
 
     action("destroy") { request in
         Todo.destroy(self.todo)
-        return self.redirectTo("/todos")
+        return redirectTo("/todos")
     }
 
     filter("setTodo") { request in
-        guard let t = Todo.find(request.params["id"]) else { return self.redirectTo("/todos") } 
+        guard let t = Todo.find(request.params["id"]) else { return redirectTo("/todos") }
         self.todo = t as? Todo
         return self.next
     }
