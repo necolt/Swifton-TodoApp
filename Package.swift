261e1b8481fa2af2159a3e6e73d7e228a3a92810
Saulius Grigaitis <saulius@necolt.com>
2016-04-12 16:58:22 +0300

    Lock PathKit version to older one

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "Swifton-TodoApp",
     dependencies: [
         .Package(url: "https://github.com/necolt/Swifton.git", versions: Version(0,0,5)..<Version(1,0,0)),
+        .Package(url: "https://github.com/necolt/PathKit.git", versions: Version(0,6,1)..<Version(0,6,2)),
         .Package(url: "https://github.com/necolt/Curassow.git", majorVersion: 0, minor: 4),
     ]
 )
